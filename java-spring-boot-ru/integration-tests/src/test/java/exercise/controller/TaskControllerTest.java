903f2069b076460b858a28fae521f21106eaa5c6
@hexlet/cli <support@hexlet.io>
2025-06-05 20:26:56 +0300

    submit java-spring-boot-ru/integration-tests

---
+++ java-spring-boot-ru/integration-tests/src/test/java/exercise/controller/TaskControllerTest.java
@@ -0,0 +1,137 @@
+package exercise.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exercise.model.Task;
+import exercise.repository.TaskRepository;
+import net.datafaker.Faker;
+import org.assertj.core.api.Assert;
+import org.instancio.Instancio;
+import org.instancio.Select;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// BEGIN
+@SpringBootTest
+@AutoConfigureMockMvc
+// END
+class TaskControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private Faker faker;
+
+	@Autowired
+	private ObjectMapper om;
+
+	@Autowired
+	private TaskRepository taskRepository;
+
+
+	@Test
+	public void testWelcomePage() throws Exception {
+		var result = mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		var body = result.getResponse().getContentAsString();
+		assertThat(body).contains("Welcome to Spring!");
+	}
+
+	@Test
+	public void testIndex() throws Exception {
+		var result = mockMvc.perform(get("/tasks"))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		var body = result.getResponse().getContentAsString();
+		assertThatJson(body).isArray();
+	}
+
+	// BEGIN
+	@Test
+	public void testShow() throws Exception {
+		var task = createTaskResord();
+		var result = mockMvc.perform(get("/tasks/{id}", task.getId()))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		var body = result.getResponse().getContentAsString();
+		assertThatJson(body).and(
+				a -> a.node("title").isEqualTo(task.getTitle()),
+				a -> a.node("description").isEqualTo(task.getDescription())
+		);
+	}
+
+	@Test
+	public void testCreate() throws Exception {
+		var request = post("/tasks")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(om.writeValueAsString(createData()));
+		var result = mockMvc.perform(request)
+				.andExpect(status().isCreated())
+				.andReturn();
+
+		var body = result.getResponse().getContentAsString();
+		assertThatJson(body).and(
+				a -> a.node("title").isEqualTo("task1"),
+				a -> a.node("description").isEqualTo("desc")
+		);
+	}
+
+	@Test
+	public void testUpdate() throws Exception {
+		var task = createTaskResord();
+		var request = put("/tasks/{id}", task.getId())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(om.writeValueAsString(createData()));
+		var result = mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andReturn();
+
+		var body = result.getResponse().getContentAsString();
+		assertThatJson(body).and(
+				a -> a.node("title").isEqualTo("task1"),
+				a -> a.node("description").isEqualTo("desc")
+		);
+	}
+
+	@Test
+	public void testDelete() throws Exception {
+		var task = createTaskResord();
+		mockMvc.perform(delete("/tasks/{id}", task.getId()))
+				.andExpect(status().isOk());
+
+		assertThat(taskRepository.findById(task.getId())).isEmpty();
+	}
+
+	public Task createTaskResord() {
+		var task = Instancio.of(Task.class)
+				.ignore(Select.field(Task::getId))
+				.supply(Select.field(Task::getTitle), () -> faker.cat().name())
+				.supply(Select.field(Task::getDescription), () -> faker.lorem().word())
+				.create();
+
+		return taskRepository.save(task);
+	}
+
+	public HashMap<String, String> createData() {
+		var data = new HashMap();
+		data.put("title", "task1");
+		data.put("description", "desc");
+
+		return data;
+	}
+	// END
+}
